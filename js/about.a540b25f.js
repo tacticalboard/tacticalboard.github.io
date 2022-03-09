77de77f401c5c5956c4cb007fc97ab595f6b2415
Mr.Blue <silverhugh.77@gmail.com>
2022-03-09 18:13:37 +0800

    Updates

---
+++ js/about.a540b25f.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{f820:function(n,t,e){"use strict";e.r(t);var a=e("7a23"),c={class:"about"},o=Object(a["e"])("h1",null,"This is an about page",-1);function s(n,t){return Object(a["o"])(),Object(a["c"])("div",c,[o])}const u={};u.render=s;t["default"]=u}}]);
+//# sourceMappingURL=about.a540b25f.js.map
